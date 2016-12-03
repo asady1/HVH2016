72adcc9d4fd0dca5414c44201739e12942af2387
Michael <michael.krohn@colorado.edu>
2016-12-03 12:51:12 -0600

    Slim miniTrees condor scripts for JetHT:

---
+++ HH/SlimMiniTrees/JetHT/runJetHT_HH_G_50.sh
@@ -0,0 +1,11 @@
+./cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc530
+scramv1 project CMSSW CMSSW_8_0_12
+cd CMSSW_8_0_12/src
+eval `scramv1 runtime -sh`
+
+cp ../../ttreeAnalyzer_hh_alphabet.py .
+
+python ttreeAnalyzer_hh_alphabet.py -f root://cmseos.fnal.gov//store/user/mkrohn/HHHHTo4b/V23/JetHT_old/JetsHT_2016G_50.root -o root://cmseos.fnal.gov//store/user/mkrohn/HH_alphabet/
+
+xrdcp JetsHT_2016G_50.root root://cmseos.fnal.gov//store/user/mkrohn/HH_alphabet/
---
+++ VH/SlimMiniTrees/JetHT/runJetHT_HH_E_924.sh
@@ -0,0 +1,11 @@
+./cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc530
+scramv1 project CMSSW CMSSW_8_0_12
+cd CMSSW_8_0_12/src
+eval `scramv1 runtime -sh`
+
+cp ../../ttreeAnalyzer_vh_alphabet_VbbTag.py .
+
+python ttreeAnalyzer_vh_alphabet_VbbTag.py -f root://cmseos.fnal.gov//store/user/mkrohn/HHHHTo4b/V23/JetHT_old/JetsHT_2016E_924.root -o root://cmseos.fnal.gov//store/user/mkrohn/VH/
+
+xrdcp JetsHT_2016E_924.root root://cmseos.fnal.gov//store/user/mkrohn/VH/

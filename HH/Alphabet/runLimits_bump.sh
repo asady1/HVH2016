2c8151db1567182ee4ca52e0abe1333200b3808e
osherson <oshersonmarc@gmail.com>
2017-01-12 16:36:22 -0600

    Update runLimits_bump.sh

--- HH/Alphabet/runLimits_bump.sh
+++ HH/Alphabet/runLimits_bump.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-for i in 1200 1400 1600 1800 2000 
+for i in 1200 
 do
-	combineCards.py Name1=outputs/bump/datacards/HH_mX_$i\_13TeV_pass.txt Name2=outputs/bump/datacards/HH_mX_$i\_13TeV_fail.txt > outputs/bump/datacards/HH_mX_$i\_13TeV.txt
+	combineCards.py Name1=outputs/datacards/HH_mX_$i\_test_13TeV_pass.txt Name2=outputs/datacards/HH_mX_$i\_test_13TeV_fail.txt > outputs/datacards/HH_mX_$i\_bump_13TeV.txt
 	echo "mass point $i"
 	mkdir Limits/sig$i
-	text2workspace.py outputs/bump/datacards/HH_mX_$i\_13TeV.txt  -o outputs/bump/datacards/HH_mX_$i\_13TeV.root
-	combine outputs/bump/datacards/HH_mX_$i\_13TeV.txt --noFitAsimov -m $i -M Asymptotic &> CMS_HH4b_$i\_13TeV_asymptoticCLs.out
+	text2workspace.py outputs/datacards/HH_mX_$i\_bump_13TeV.txt  -o outputs/datacards/HH_mX_$i\_bump_13TeV.root
+	combine outputs/datacards/HH_mX_$i\_bump_13TeV.txt --noFitAsimov -m $i -M Asymptotic &> CMS_HH4b_$i\_13TeV_asymptoticCLs.out
 	mv higgsCombineTest.Asymptotic.mH$i\.root Limits/CMS_$i\_HH4b_13TeV_asymptoticCLs.root
+	combine -M MaxLikelihoodFit --rMin=-100 --rMax=100 --saveNormalizations --plot --saveShapes --saveWithUncertainties  -v 4 outputs/datacards/HH_mX_$i\_bump_13TeV.txt --out outputs/datacards
 done
